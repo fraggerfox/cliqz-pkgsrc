ae86188e808dcda0f96122f3d95edb105c5833cd
fraggerfox <santhosh.raju@gmail.com>
2020-04-17 04:40:36 -0500

    Updates the package from cliqz-1.34.1 to cliqz-1.35.0.

---
+++ www/cliqz/patches/patch-js_src_jsfriendapi.h
@@ -0,0 +1,16 @@
+$NetBSD: patch-js_src_jsfriendapi.h,v 1.1 2020/04/09 14:01:26 ryoon Exp $
+
+* Fix va_list error.
+
+Taken from www/firefox
+
+--- mozilla-release/js/src/jsfriendapi.h.orig	2020-04-03 19:34:47.000000000 +0000
++++ mozilla-release/js/src/jsfriendapi.h
+@@ -13,6 +13,7 @@
+ #include "mozilla/MemoryReporting.h"
+ #include "mozilla/UniquePtr.h"
+ 
++#include <stdarg.h>
+ #include "jspubtd.h"
+ 
+ #include "js/CallArgs.h"
